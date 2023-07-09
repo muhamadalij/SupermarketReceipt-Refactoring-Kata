cfa4d598dde6565a3354c942c3e8db9af888da29
muhammad ali Jalali <seyed.ma.jalali@gmail.com>
2023-07-09 14:46:03 +0330

    change 2/5: some funcs do not need public access, make them private to have better encapsulation!

--- cpp/src/ReceiptPrinter.h
+++ cpp/src/ReceiptPrinter.h
@@ -8,19 +8,24 @@
 class ReceiptPrinter
 {
 public:
+    
     ReceiptPrinter();
     ReceiptPrinter(int columns);
+    
     std::string printReceipt(const Receipt &receipt);
+
+private:    
+
     std::string presentReceiptItem(const ReceiptItem &item) const;
     std::string presentDiscount(const Discount &discount) const;
     std::string presentTotal(const Receipt &receipt) const;
     std::string formatLineWithWhitespace(const std::string &name, const std::string &value) const;
     std::string presentPrice(double price) const;
+    
     static std::string presentQuantity(const ReceiptItem &item);
-
-private:
     static std::string getFormattedNumberAsString(double number, int precision);
+    
     const int columns;
 };
 
-#endif // CPP_RECEIPTPRINTER_H
+#endif //CPP_RECEIPTPRINTER_H
